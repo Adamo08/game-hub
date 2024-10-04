5e3662211a7fdeeb1d278197303ff78e32e259fe
Adamo0 <you@example.com>
2024-10-05 00:56:24 +0200

    Moving the fetching logic to a custom hook useGames()

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { Controller } from "react-hook-form";
+import { CanceledError } from "axios";
+
+interface Game{
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count : number;
+    results: Game[]
+
+}
+
+
+const  useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+
+    useEffect(()=>{
+
+        const controller = new AbortController();
+
+        apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
+        .then(res => setGames(res.data.results))
+        .catch(err => {
+            if (err instanceof CanceledError) return;
+            setError(err.message);
+        });
+
+        return () => controller.abort(); 
+
+    }, [])
+
+    return { games, error }
+}
+
+export default useGames;
