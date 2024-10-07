65520c8907128ba0dc3d9113cb70db919c68177a
Adamo0 <you@example.com>
2024-10-07 18:05:59 +0200

    Added Emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCroppedImageUrl from '../services/image-url'
+import Emoji from './Emoji'
 
 
 interface Props {
@@ -21,7 +22,11 @@ const GameCard = ({ game } : Props) => {
                     <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
                     <CriticScore score={game.metacritic}/>
                 </HStack>
-                <Heading fontSize={'2xl'}>{game.name}</Heading>
+                <Heading 
+                    fontSize={'2xl'}
+                >
+                    {game.name} <Emoji rating={game.rating_top} />
+                </Heading>
             </CardBody>
         </Card>
     )
