56176ced648351cf62acb3395f4b3be4e00704ff
Adamo0 <you@example.com>
2024-10-07 18:31:49 +0200

    Customizing the theme for darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -10,6 +10,24 @@ const config: ThemeConfig = {
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ 
+    config,
+    colors:{
+        gray:{
+            50:  '#F9F9F9',
+            100: '#EDEDED',
+            200: '#D3D3D3',
+            300: '#B3B3B3',
+            400: '#A0A0A0',
+            500: '#898989',
+            600: '#636363',
+            700: '#202020',
+            800: '#121212',
+            900: '#111111',
+
+
+        }
+    }
+})
 
 export default theme
